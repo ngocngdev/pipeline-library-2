6b60f75e23f51ab63b20a937bb484cdb9ba44984
jguzman4 <jguzman23@mail.csuchico.edu>
2018-01-22 09:57:03 -0800

    spliting the build functions for maven build and npm build

---
+++ vars/buildMvn.groovy
@@ -0,0 +1,10 @@
+/*
+ * This is a shared library function intended to be used during the build stage
+ * of a pipeline. 
+ *
+ */
+
+def call() {
+
+
+}
---
+++ vars/buildNpm.groovy
@@ -0,0 +1,11 @@
+/*
+ * This is a shared library function intended to be used during the build stage
+ * of a pipeline. 
+ *
+ */
+
+def call() {
+
+
+}
+
